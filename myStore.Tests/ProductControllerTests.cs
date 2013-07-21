4391f7853084f95837b8d0d9415eb90206f5c259
Gursharn Singh <gursharnsinghkhalsa@googlemail.com>
2013-07-21 09:26:26 -0400

    Initial commit

---
+++ myStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myStore.Tests
+{
+    [TestClass]
+    public class ProductControllerTests
+    {
+        [TestMethod]
+        public void should_be_able_to_see_list_of_all_products()
+        {
+            
+        }
+
+        [TestMethod]
+        public void should_be_able_to_see_product_details()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_a_valid_product()
+        {
+        }
+
+        [TestMethod]
+        public void should_not_be_able_to_add_a_invalid_product()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_edit_a_product()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_delete_a_product()
+        {
+        }
+    }
+}
---
+++ myStore.Tests/ShoppingCartControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myStore.Tests
+{
+    [TestClass]
+    public class ShoppingCartControllerTests
+    {
+        [TestMethod]
+        public void should_be_able_to_see_shopping_cart_details()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_add_a_product_to_shopping_cart()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_remove_a_product_from_shopping_cart()
+        {
+        }
+
+        [TestMethod]
+        public void should_be_able_to_update_product_quantity()
+        {
+        }
+    }
+}
